831382ce328c139bb29472b771e0c33f744a52e6
John Vandenberg <jayvdb@gmail.com>
2020-09-16 08:53:11 +0700

    models: Use get_username
    get_username respects USERNAME_FIELD.

--- permissions/models.py
+++ permissions/models.py
@@ -183,7 +183,7 @@ class PrincipalRoleRelation(models.Model):
 
     def __unicode__(self):
         if self.user:
-            principal = self.user.username
+            principal = self.user.get_username()
         else:
             principal = self.group
 
